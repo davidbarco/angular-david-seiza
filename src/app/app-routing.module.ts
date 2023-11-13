04cc6d649b069594fd17c122ef112f0cb2d1784c
davidbarco <66650959+davidbarco@users.noreply.github.com>
2023-11-13 14:51:57 -0300

    se crea guardian de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavLayoutComponent } from './shared/components/nav-layout/nav-layout.component';
+import { UserIsLoggedIn, UserNotLoggedIn } from './shared/guards/guards.guard';
 
 
 const routes: Routes = [
   {
     path: '',
-    //canActivate: [UserIsNotLoggedIn],
+    canActivate: [UserNotLoggedIn],
     loadChildren: () => import('./views/sessions/sessions.module').then((m) => m.SessionsModule),
   },
   {
     path: 'app',
-    //canActivate: [UserIsLoggedIn],
+    canActivate: [UserIsLoggedIn],
     component: NavLayoutComponent,
     children: [
       {
